4e7a52389e8d522339725e7e834e0056a7e6b42b
GitHub Action <action@github.com>
2023-08-30 19:05:15 +0000

    Add version.ts file

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 export const PACKAGE_NAME: string = "html-living-framework";
 export const PACKAGE_VERSION: string = "2.0.4";
-export const GIT_BRANCH: string | undefined = "main";
-export const GIT_COMMIT: string | undefined = "063e00d71d932008481fa32a3bd582364c8e5d22";
+export const GIT_BRANCH: string = "main";
+export const GIT_COMMIT: string = "b562ced12712adfb7c854628f7169bfc3b1f57f6";
