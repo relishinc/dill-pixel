83a85ad43cef089df63b30a545ef36f6438895e1
anthonysapp <anthony@reli.sh>
2023-08-30 14:46:09 -0400

    feat: adding the creation and saving of version.ts to the release-please workflow

--- src/Load/Assets/AssetFactory.ts
+++ src/Load/Assets/AssetFactory.ts
@@ -8,9 +8,9 @@ export type AssetFactoryFunction<T> = (id: string | number, resolutionSuffix?: s
  * Asset factory is used to generate list of assets based on pattern
  *
  * @param assetIndex - index of asset: AssetFactory(1, i => new TextureAsset("my-texture-" + i))
- * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
+ * @param factory - function that returns an asset by its id from the pattern and resolution suffix
  */
-export function AssetFactory<T>(assetIndex: number, factory: AssetFactoryFunction<T>): Array<IAsset<T>>;
+export function AssetFactory<T>(assetIndex: number, factory: AssetFactoryFunction<T>): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -18,14 +18,14 @@ export function AssetFactory<T>(assetIndex: number, factory: AssetFactoryFunctio
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  * @param resolutionSuffix - resolution suffix of assets
  */
-export function AssetFactory<T>(assetIndex: number, factory: AssetFactoryFunction<T>, resolutionSuffix: string): Array<IAsset<T>>;
+export function AssetFactory<T>(assetIndex: number, factory: AssetFactoryFunction<T>, resolutionSuffix: string): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
  * @param assetName - name of asset: AssetFactory("my-texture", i => new TextureAsset(i))
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  */
-export function AssetFactory<T>(assetName: string, factory: AssetFactoryFunction<T>): Array<IAsset<T>>;
+export function AssetFactory<T>(assetName: string, factory: AssetFactoryFunction<T>): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -33,14 +33,14 @@ export function AssetFactory<T>(assetName: string, factory: AssetFactoryFunction
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  * @param resolutionSuffix - resolution suffix of assets
  */
-export function AssetFactory<T>(assetName: string, factory: AssetFactoryFunction<T>, resolutionSuffix: string): Array<IAsset<T>>;
+export function AssetFactory<T>(assetName: string, factory: AssetFactoryFunction<T>, resolutionSuffix: string): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
  * @param minMax - min and max index of asset: AssetFactory([1,3], i => new TextureAsset("my-texture-" + i))
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  */
-export function AssetFactory<T>(minMax: [number, number], factory: AssetFactoryFunction<T>): Array<IAsset<T>>;
+export function AssetFactory<T>(minMax: [number, number], factory: AssetFactoryFunction<T>): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -48,14 +48,14 @@ export function AssetFactory<T>(minMax: [number, number], factory: AssetFactoryF
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  * @param resolutionSuffix - resolution suffix of assets
  */
-export function AssetFactory<T>(minMax: [number, number], factory: AssetFactoryFunction<T>, resolutionSuffix: string): Array<IAsset<T>>;
+export function AssetFactory<T>(minMax: [number, number], factory: AssetFactoryFunction<T>, resolutionSuffix: string): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
  * @param assetNames - list of asset names: AssetFactory(["a","b","c"], i => new TextureAsset("my-texture-" + i))
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  */
-export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunction<T>): Array<IAsset<T>>;
+export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunction<T>): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -63,7 +63,7 @@ export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunct
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  * @param resolutionSuffix - resolution suffix of assets
  */
-export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunction<T>, resolutionSuffix: string): Array<IAsset<T>>;
+export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunction<T>, resolutionSuffix: string): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -72,7 +72,7 @@ export function AssetFactory<T>(assetNames: string[], factory: AssetFactoryFunct
  * @param factory - function that returns an asset by it's id from the pattern and resolution suffix
  */
 export function AssetFactory<T>(patternsByResolution: { [resolutionSuffix: string]: AssetFactoryPattern },
-                                factory: AssetFactoryFunction<T>): Array<IAsset<T>>;
+                                factory: AssetFactoryFunction<T>): IAsset<T>[];
 /**
  * Asset factory is used to generate list of assets based on pattern
  *
@@ -82,41 +82,41 @@ export function AssetFactory<T>(patternsByResolution: { [resolutionSuffix: strin
  * @param resolutionSuffix - resolution suffix of assets
  */
 export function AssetFactory<T>(patternsByResolution: { [resolutionSuffix: string]: AssetFactoryPattern },
-                                factory: AssetFactoryFunction<T>, resolutionSuffix: string): Array<IAsset<T>>;
+                                factory: AssetFactoryFunction<T>, resolutionSuffix: string): IAsset<T>[];
 export function AssetFactory<T>(
-    pattern: AssetFactoryPattern | { [resolutionSuffix: string]: AssetFactoryPattern },
-    factory: AssetFactoryFunction<T>,
-    resolutionSuffix?: string): Array<IAsset<T>> {
+	pattern: AssetFactoryPattern | { [resolutionSuffix: string]: AssetFactoryPattern },
+	factory: AssetFactoryFunction<T>,
+	resolutionSuffix?: string): IAsset<T>[] {
 
-    if (pattern && factory) {
-        if (typeof pattern === "string" || typeof pattern === "number") {
-            const asset = factory(pattern, resolutionSuffix);
-            asset.resolutionSuffix = resolutionSuffix || "";
-            return [asset];
-        }
-        const assets = [];
-        if (Array.isArray(pattern)) {
-            if (pattern.length === 2 && typeof pattern[0] === "number" && typeof pattern[1] === "number") {
-                for (let i = pattern[0], n = pattern[1]; i <= n; i++) {
-                    const asset = factory(i, resolutionSuffix);
-                    asset.resolutionSuffix = resolutionSuffix || "";
-                    assets.push(asset);
-                }
-            } else {
-                for (const p of pattern) {
-                    const asset = factory(p, resolutionSuffix);
-                    asset.resolutionSuffix = resolutionSuffix || "";
-                    assets.push(asset);
-                }
-            }
-        } else {
-            for (const r in pattern) {
-                if (pattern.hasOwnProperty(r)) {
-                    assets.push(...AssetFactory(pattern[r] as any, factory, r));
-                }
-            }
-        }
-        return assets;
-    }
-    return [];
+	if (pattern && factory) {
+		if (typeof pattern === "string" || typeof pattern === "number") {
+			const asset = factory(pattern, resolutionSuffix);
+			asset.resolutionSuffix = resolutionSuffix || "";
+			return [asset];
+		}
+		const assets = [];
+		if (Array.isArray(pattern)) {
+			if (pattern.length === 2 && typeof pattern[0] === "number" && typeof pattern[1] === "number") {
+				for (let i = pattern[0], n = pattern[1]; i <= n; i++) {
+					const asset = factory(i, resolutionSuffix);
+					asset.resolutionSuffix = resolutionSuffix || "";
+					assets.push(asset);
+				}
+			} else {
+				for (const p of pattern) {
+					const asset = factory(p, resolutionSuffix);
+					asset.resolutionSuffix = resolutionSuffix || "";
+					assets.push(asset);
+				}
+			}
+		} else {
+			for (const r in pattern) {
+				if (pattern.hasOwnProperty(r)) {
+					assets.push(...AssetFactory(pattern[r] as any, factory, r));
+				}
+			}
+		}
+		return assets;
+	}
+	return [];
 }
