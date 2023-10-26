a74190d2c2228cc75b18beae6932bdb5ce770f9e
anthonysapp <anthony@reli.sh>
2023-10-26 16:24:46 -0400

    docs: logo & Setup Guide, colour scheme first attempt

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,5 +1,19 @@
-const colors = require('tailwindcss/colors');
 const starlightPlugin = require('@astrojs/starlight-tailwind');
+
+
+// Generated color palettes
+const accent = {200: '#abd79d', 600: '#2f8000', 900: '#123d00', 950: '#0b2c00'};
+const gray = {
+	100: '#f2f7f9',
+	200: '#e6f0f3',
+	300: '#b9c4c8',
+	400: '#7a9097',
+	500: '#475c63',
+	700: '#283c42',
+	800: '#172a30',
+	900: '#111a1c'
+};
+
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,9 +22,9 @@ module.exports = {
 		extend: {
 			colors: {
 				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.lime,
+				accent,
 				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
+				gray
 			},
 		},
 	},
