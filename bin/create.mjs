fe4754fd42e297515b753421aded480828b8caf5
anthonysapp <anthony@reli.sh>
2023-10-27 12:56:39 -0400

    refactor: command line dill-pixel create method

--- bin/create.mjs
+++ bin/create.mjs
@@ -1,95 +1,124 @@
 #!/usr/bin/env node
 import * as p from '@clack/prompts';
-import {bold, cyan, green} from 'kleur/colors';
+import {bold, cyan} from 'kleur/colors';
 import fs from 'node:fs';
 import path from 'node:path';
-import {create} from './index.mjs';
-import {dist, package_manager} from './utils.mjs';
+import {copy, dist, mkdirp, package_manager} from './utils.mjs';
 
-const currentVersion = process.versions.node;
-const requiredMajorVersion = parseInt(currentVersion.split('.')[0], 10);
-const minimumMajorVersion = 18;
-
-if (requiredMajorVersion < minimumMajorVersion) {
-	console.error(`Node.js v${currentVersion} is out of date and unsupported!`);
-	console.error(`Please use Node.js v${minimumMajorVersion} or higher.`);
-	process.exit(1);
+async function write(cwd, options) {
+	mkdirp(cwd);
+	write_template_files(options.template, options.name, cwd);
 }
 
-const {version} = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf-8'));
-let cwd = process.argv[2] || '.';
+function getFiles(dirPath) {
+	let files = [];
 
-console.log(`
-${green(`Dill Pixel ${version}`)}
-`);
-
-p.intro('This utility will walk you through creating a new Dill Pixel project.');
-
-if (cwd === '.') {
-	const dir = await p.text({
-		message: 'Where should we create your project?',
-		placeholder: '  (hit Enter to use current directory)',
+	const dirFiles = fs.readdirSync(dirPath, {withFileTypes: true});
+	dirFiles.forEach((file) => {
+		const name = file.name;
+		if (fs.statSync(path.join(dirPath, name)).isDirectory()) {
+			files = files.concat(getFiles(path.join(dirPath, name)));
+		} else {
+			files.push({name: path.join(dirPath, name)});
+		}
 	});
 
-	if (p.isCancel(dir)) {
-		process.exit(1);
-	}
-
-	if (dir) {
-		cwd = /** @type {string} */ (dir);
-	}
+	return files;
 }
 
-if (fs.existsSync(cwd)) {
-	if (fs.readdirSync(cwd).length > 0) {
-		const force = await p.confirm({
-			message: 'Directory not empty. Continue?',
-			initialValue: false,
+/**
+ * @param {string} template
+ * @param {'typescript' | 'checkjs' | null} types
+ * @param {string} name
+ * @param {string} cwd
+ */
+function write_template_files(template, name, cwd) {
+	const dir = dist(`templates/${template}`);
+	copy(`${dir}/package.template.json`, `${cwd}/package.json`);
+
+	const dirPath = `${dir}`;
+	const files = getFiles(dirPath);
+	files.forEach((file) => {
+		const dest = path.join(cwd, file.name.replace(dirPath, ''));
+		copy(file.name, dest);
+	});
+
+	const pkg = fs.readFileSync(`${cwd}/package.json`, 'utf-8');
+	fs.writeFileSync(`${cwd}/package.json`, pkg.replace(/~TODO~/g, name));
+	try {
+		fs.rmSync(`${cwd}/.meta.json`);
+	} catch (e) {}
+}
+
+export async function create(cwd) {
+	p.intro('This utility will walk you through creating a new Dill Pixel project.');
+	
+	if (cwd === '.') {
+		const dir = await p.text({
+			message: 'Where should we create your project?',
+			placeholder: '  (hit Enter to use current directory)',
 		});
 
-		// bail if `force` is `false` or the user cancelled with Ctrl-C
-		if (force !== true) {
+		if (p.isCancel(dir)) {
 			process.exit(1);
 		}
+
+		if (dir) {
+			cwd = /** @type {string} */ (dir);
+		}
 	}
-}
 
-const options = await p.group({
-	template: () =>
-		p.select({
-			message: 'Which template?',
-			// @ts-expect-error i have no idea what is going on here
-			options: fs.readdirSync(dist('templates')).map((dir) => {
-				const meta_file = dist(`templates/${dir}/.meta.json`);
-				const {title, description} = JSON.parse(fs.readFileSync(meta_file, 'utf8'));
+	if (fs.existsSync(cwd)) {
+		if (fs.readdirSync(cwd).length > 0) {
+			const force = await p.confirm({
+				message: 'Directory not empty. Continue?',
+				initialValue: false,
+			});
 
-				return {
-					label: title,
-					hint: description,
-					value: dir,
-				};
+			// bail if `force` is `false` or the user cancelled with Ctrl-C
+			if (force !== true) {
+				process.exit(1);
+			}
+		}
+	}
+
+	const options = await p.group({
+		template: () =>
+			p.select({
+				message: 'Which template?',
+				// @ts-expect-error i have no idea what is going on here
+				options: fs.readdirSync(dist('templates')).map((dir) => {
+					const meta_file = dist(`templates/${dir}/.meta.json`);
+					const {title, description} = JSON.parse(fs.readFileSync(meta_file, 'utf8'));
+
+					return {
+						label: title,
+						hint: description,
+						value: dir,
+					};
+				}),
 			}),
-		}),
-});
+	});
 
-await create(cwd, {
-	name: path.basename(path.resolve(cwd)),
-	template: /** @type {'default' | 'react'} */ (options.template),
-});
+	await write(cwd, {
+		name: path.basename(path.resolve(cwd)),
+		template: /** @type {'default' | 'react'} */ (options.template),
+	});
 
-p.outro('Your project is ready!');
+	p.outro('Your project is ready!');
 
-console.log('\nNext steps:');
-let i = 1;
+	console.log('\nNext steps:');
+	let i = 1;
 
-const relative = path.relative(process.cwd(), cwd);
-if (relative !== '') {
-	console.log(`  ${i++}: ${bold(cyan(`cd ${relative}`))}`);
+	const relative = path.relative(process.cwd(), cwd);
+	if (relative !== '') {
+		console.log(`  ${i++}: ${bold(cyan(`cd ${relative}`))}`);
+	}
+
+	console.log(`  ${i++}: ${bold(cyan(`${package_manager} install`))}`);
+	// prettier-ignore
+	console.log(`  ${i++}: ${bold(cyan('git init && git add -A && git commit -m "Initial commit"'))} (optional)`);
+	console.log(`  ${i++}: ${bold(cyan(`${package_manager} run dev -- --open`))}`);
+
+	console.log(`\nTo close the dev server, hit ${bold(cyan('Ctrl-C'))}`);
 }
-
-console.log(`  ${i++}: ${bold(cyan(`${package_manager} install`))}`);
-// prettier-ignore
-console.log(`  ${i++}: ${bold(cyan('git init && git add -A && git commit -m "Initial commit"'))} (optional)`);
-console.log(`  ${i++}: ${bold(cyan(`${package_manager} run dev -- --open`))}`);
-
-console.log(`\nTo close the dev server, hit ${bold(cyan('Ctrl-C'))}`);
