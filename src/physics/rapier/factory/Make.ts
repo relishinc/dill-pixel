90967cc5166f1f0e2b21cc7c27b301d5f8a27425
anthonysapp <anthony@reli.sh>
2023-11-07 14:55:08 -0500

    fix: some settings objects assumed properties were set

--- src/physics/rapier/factory/Make.ts
+++ src/physics/rapier/factory/Make.ts
@@ -20,7 +20,7 @@ export class Make {
     settingsOrAsset?: string | Texture | PhysicsSpriteSettings,
     sheet?: SpritesheetLike,
     size?: PointLike,
-    bodyType?: PhysicsBodyType,
+    bodyType: PhysicsBodyType = PhysicsBodyType.RECTANGLE,
     alpha: number = 1,
     position: PointLike = 0,
     scale: PointLike = 1,
@@ -29,14 +29,14 @@ export class Make {
     let asset = settingsOrAsset as string | Texture | undefined;
     if (typeof settingsOrAsset === 'object' && !(settingsOrAsset instanceof Texture)) {
       const settings = settingsOrAsset as PhysicsSpriteSettings;
-      asset = settings.asset;
-      sheet = settings.sheet;
-      size = settings.size;
-      bodyType = settings.bodyType;
-      alpha = settings.alpha ?? 1;
-      position = settings.position ?? 0;
-      scale = settings.scale ?? 1;
-      visible = settings.visible !== false;
+      asset = settings?.asset;
+      sheet = settings?.sheet;
+      size = settings?.size ?? 1;
+      bodyType = settings?.bodyType ?? PhysicsBodyType.RECTANGLE;
+      alpha = settings?.alpha ?? 1;
+      position = settings?.position ?? 0;
+      scale = settings?.scale ?? 1;
+      visible = settings?.visible !== false;
     }
     if (!asset) {
       asset = Texture.WHITE;
