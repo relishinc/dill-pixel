a709c04e7790d5cfeb47039ac3cff0103179aa68
Steve Palmer <steve@reli.sh>
2023-11-24 13:49:09 -0800

    feat: add texture helper for radial gradients

--- src/utils/TextureUtils.ts
+++ src/utils/TextureUtils.ts
@@ -1,6 +1,6 @@
 import { Texture, TextureSource } from 'pixi.js';
 
-export function createGradientTexture(
+export function createLinearGradientTexture(
   width: number,
   colorStops: {
     offset: number;
@@ -22,6 +22,35 @@ export function createGradientTexture(
 
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, width, 1);
+
+    return Texture.from(canvas as TextureSource);
+  }
+}
+
+export const createGradientTexture = createLinearGradientTexture;
+
+export function createRadialGradientTexture(
+  radius: number,
+  colorStops: {
+    offset: number;
+    color: string;
+  }[],
+) {
+  const canvas = document.createElement('canvas');
+  canvas.width = radius * 2;
+  canvas.height = radius * 2;
+
+  const ctx = canvas.getContext('2d');
+  if (ctx) {
+    // use canvas2d API to create gradient
+    const grd = ctx.createRadialGradient(radius, radius, 0, radius, radius, radius * 2);
+
+    colorStops.forEach((cs) => {
+      grd.addColorStop(cs.offset, cs.color);
+    });
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, radius * 2, radius * 2);
 
     return Texture.from(canvas as TextureSource);
   }
