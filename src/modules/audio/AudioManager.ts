c60960a7bf0037a972fce6a1aa66479c8aef715f
anthonysapp <anthony@reli.sh>
2024-04-23 12:32:48 -0400

    chore: comments in AudioManager

--- src/modules/audio/AudioManager.ts
+++ src/modules/audio/AudioManager.ts
@@ -66,13 +66,41 @@ export interface IAudioManager extends IModule {
   restore(): void;
 }
 
+/**
+ * AudioManager is a class that manages audio playback in the application.
+ * It provides methods to play, stop, fade in/out, and crossfade sounds.
+ * It also allows you to create and manage audio channels.
+ *
+ * @example
+ * const audioManager = new AudioManager();
+ * audioManager.play('soundId', 'music');
+ */
 @CoreModule
 export class AudioManager extends Module implements IAudioManager {
   // signals
+  /**
+   * Signal that is emitted when a sound starts playing.
+   * The callback function receives a SoundDetail object.
+   */
   public onSoundStarted: Signal<(detail: SoundDetail) => void> = new Signal<(detail: SoundDetail) => void>();
+  /**
+   * Signal that is emitted when a sound ends.
+   * The callback function receives a SoundDetail object.
+   */
   public onSoundEnded: Signal<(detail: SoundDetail) => void> = new Signal<(detail: SoundDetail) => void>();
+  /**
+   * Signal that is emitted when the system is muted or unmuted.
+   */
   public onMuted: Signal<(muted: boolean) => void> = new Signal<(muted: boolean) => void>();
+  /**
+   * Signal that is emitted when the master volume changes.
+   * The callback function receives the new volume as a number.
+   */
   public onMasterVolumeChanged: Signal<(volume: number) => void> = new Signal<(volume: number) => void>();
+  /**
+   * Signal that is emitted when a channel's volume changes.
+   * The callback function receives a ChannelVolumeDetail object.
+   */
   public onChannelVolumeChanged: Signal<(detail: ChannelVolumeDetail) => void> = new Signal<
     (detail: ChannelVolumeDetail) => void
   >();
@@ -83,6 +111,10 @@ export class AudioManager extends Module implements IAudioManager {
   private _paused: boolean = false;
   private _channels: Map<string, IAudioChannel> = new Map();
 
+  /**
+   * Creates a new AudioManager instance.
+   * @param {string} id - The ID of the AudioManager. Default is 'AudioManager'.
+   */
   public constructor(id: string = 'AudioManager') {
     super(id);
     this.createChannel('music');
@@ -90,23 +122,43 @@ export class AudioManager extends Module implements IAudioManager {
     this.createChannel('voiceover');
   }
 
+  /**
+   * Gets the master volume.
+   * @returns {number} The master volume.
+   */
   get masterVolume(): number {
     return this._masterVolume;
   }
 
+  /**
+   * Sets the master volume.
+   * @param {number} value - The new master volume.
+   */
   set masterVolume(value: number) {
     this._masterVolume = value;
     this._channels.forEach((channel) => channel.updateVolume());
   }
 
+  /**
+   * Gets the map of audio channels.
+   * @returns {Map<string, IAudioChannel>} The map of audio channels.
+   */
   get channels(): Map<string, IAudioChannel> {
     return this._channels;
   }
 
+  /**
+   * Gets whether the audio is muted.
+   * @returns {boolean} True if the audio is muted, false otherwise.
+   */
   get muted(): boolean {
     return this._muted;
   }
 
+  /**
+   * Sets whether the audio is muted.
+   * @param {boolean} value - True to mute the audio, false to unmute.
+   */
   set muted(value: boolean) {
     this._muted = value;
     this._setMuted();
@@ -126,6 +178,11 @@ export class AudioManager extends Module implements IAudioManager {
     super.destroy();
   }
 
+  /**
+   * Initializes the AudioManager.
+   * @param {IApplication} app
+   * @returns {Promise<void>}
+   */
   public initialize(app: IApplication): Promise<void> {
     Logger.log('AudioManager initialized', app);
     if (typeof app?.manifest === 'object') {
@@ -134,6 +191,10 @@ export class AudioManager extends Module implements IAudioManager {
     return Promise.resolve(undefined);
   }
 
+  /**
+   * Creates a new audio channel.
+   * @param {string} name
+   */
   public createChannel(name: string): void {
     if (this._channels.has(name)) {
       throw new Error(`Channel with name ${name} already exists.`);
@@ -142,6 +203,11 @@ export class AudioManager extends Module implements IAudioManager {
     this._channels.set(name, channel);
   }
 
+  /**
+   * Sets the volume of the specified channel.
+   * @param {ChannelName | ChannelName[]} channelName
+   * @param {number} volume
+   */
   public setChannelVolume(channelName: ChannelName | ChannelName[], volume: number): void {
     if (!Array.isArray(channelName)) {
       channelName = [channelName];
@@ -149,36 +215,62 @@ export class AudioManager extends Module implements IAudioManager {
     channelName.forEach((name) => this._setChannelVolume(name, volume));
   }
 
+  /**
+   * Gets the audio channel with the specified name.
+   * @param {ChannelName} name
+   * @returns {IAudioChannel | undefined}
+   */
   public getChannel(name: ChannelName): IAudioChannel | undefined {
     return this._channels.get(name);
   }
 
+  /**
+   * Mutes the audio.
+   */
   public mute() {
     this._muted = true;
     this._setMuted();
   }
 
+  /**
+   * Unmutes the audio.
+   */
   public unmute() {
     this._muted = false;
     this._setMuted();
   }
 
+  /**
+   * Pauses the audio.
+   */
   public pause(): void {
     this._paused = true;
     this._setPaused();
   }
 
+  /**
+   * Resumes the audio.
+   */
   public resume(): void {
     this._paused = false;
     this._setPaused();
   }
 
+  /**
+   * Adds all sound assets from the specified manifest.
+   * @param {AssetsManifest} manifest
+   */
   public addAllFromManifest(manifest: AssetsManifest) {
     manifest.bundles.forEach((bundle) => {
       this.addAllFromBundle(bundle.name, manifest);
     });
   }
 
+  /**
+   * Adds all sound assets from the specified bundle.
+   * @param {string} bundleName
+   * @param {AssetsManifest | string | undefined} manifest
+   */
   public addAllFromBundle(bundleName: string, manifest?: AssetsManifest | string | undefined) {
     if (!manifest) {
       manifest = this.app.manifest;
@@ -206,6 +298,10 @@ export class AudioManager extends Module implements IAudioManager {
     });
   }
 
+  /**
+   * Adds a sound asset to the AudioManager.
+   * @param {UnresolvedAsset} soundAsset
+   */
   public add(soundAsset: UnresolvedAsset): void {
     let alias = soundAsset.alias;
     if (!Array.isArray(soundAsset.alias)) {
@@ -224,6 +320,13 @@ export class AudioManager extends Module implements IAudioManager {
     }
   }
 
+  /**
+   * Plays a sound with the specified ID in the specified channel.
+   * @param {string} soundId
+   * @param {ChannelName} channelName
+   * @param {PlayOptions} options
+   * @returns {Promise<IAudioInstance>}
+   */
   public async play(soundId: string, channelName: ChannelName = 'sfx', options?: PlayOptions): Promise<IAudioInstance> {
     const channel = this._channels.get(channelName);
     Logger.log('play', soundId, channelName, options, channel);
@@ -241,6 +344,12 @@ export class AudioManager extends Module implements IAudioManager {
     }
   }
 
+  /**
+   * Stops a sound with the specified ID in the specified channel.
+   * @param {string} soundId
+   * @param {ChannelName} channelName
+   * @returns {IAudioInstance | undefined}
+   */
   public stop(soundId: string, channelName: ChannelName = 'sfx'): IAudioInstance | undefined {
     Logger.log('stop', soundId, channelName);
     const channel = this._channels.get(channelName);
@@ -251,6 +360,13 @@ export class AudioManager extends Module implements IAudioManager {
     }
   }
 
+  /**
+   * Fades in a sound with the specified ID in the specified channel.
+   * @param {string} soundId
+   * @param {ChannelName} channelName
+   * @param {gsap.TweenVars} props
+   * @returns {Promise<gsap.core.Tween | null>}
+   */
   public async fadeIn(
     soundId: string,
     channelName: ChannelName = 'music',
@@ -267,6 +383,13 @@ export class AudioManager extends Module implements IAudioManager {
     return this.fade(soundId, channelName, fadeProps);
   }
 
+  /**
+   * Fades out a sound with the specified ID in the specified channel.
+   * @param {string} soundId
+   * @param {ChannelName} channelName
+   * @param {Partial<gsap.TweenVars>} props
+   * @returns {Promise<gsap.core.Tween | null>}
+   */
   public async fadeOut(
     soundId: string,
     channelName: ChannelName = 'music',
@@ -285,6 +408,14 @@ export class AudioManager extends Module implements IAudioManager {
     return this.fade(soundId, channelName, fadeProps, true);
   }
 
+  /**
+   * Crossfades between two sounds in the specified channel.
+   * @param {string} outSoundId
+   * @param {string} inSoundId
+   * @param {ChannelName} channelName
+   * @param {number} duration
+   * @returns {Promise<gsap.core.Tween | null>}
+   */
   public async crossFade(
     outSoundId: string,
     inSoundId: string,
@@ -296,6 +427,14 @@ export class AudioManager extends Module implements IAudioManager {
     return this.fadeIn(inSoundId, channelName, crossFadeProps);
   }
 
+  /**
+   * Fades a sound with the specified ID in the specified channel.
+   * @param {string} soundId
+   * @param {ChannelName} channelName
+   * @param {gsap.TweenVars} props
+   * @param {boolean} stopOnComplete
+   * @returns {Promise<gsap.core.Tween | null>}
+   */
   public async fade(
     soundId: string,
     channelName: ChannelName = 'music',
@@ -316,6 +455,9 @@ export class AudioManager extends Module implements IAudioManager {
     return null;
   }
 
+  /**
+   * Restores the audio state after it has been suspended.
+   */
   public restore() {
     if (this._storedVolume !== undefined) {
       this.masterVolume = this._storedVolume;
@@ -324,12 +466,18 @@ export class AudioManager extends Module implements IAudioManager {
     this.resume();
   }
 
+  /**
+   * Suspends the audio by setting the master volume to 0 and pausing all sounds.
+   */
   public suspend() {
     this._storedVolume = this._masterVolume;
     this.masterVolume = 0;
     this.pause();
   }
 
+  /**
+   * @private
+   */
   private _setMuted(): void {
     this._channels.forEach((channel) => {
       channel.muted = this._muted;
@@ -342,6 +490,9 @@ export class AudioManager extends Module implements IAudioManager {
     this.onMuted.emit(this._muted);
   }
 
+  /**
+   * @private
+   */
   private _setPaused(): void {
     if (this._paused) {
       sound.pauseAll();
@@ -350,6 +501,12 @@ export class AudioManager extends Module implements IAudioManager {
     }
   }
 
+  /**
+   * Sets the volume of the specified channel.
+   * @param {ChannelName} channelName
+   * @param {number} volume
+   * @private
+   */
   private _setChannelVolume(channelName: ChannelName, volume: number): void {
     const channel = this._channels.get(channelName);
     if (channel) {
@@ -359,11 +516,25 @@ export class AudioManager extends Module implements IAudioManager {
     }
   }
 
+  /**
+   * Sound started event handler. Emit the onSoundStarted signal.
+   * @param {string} id
+   * @param {IAudioInstance} instance
+   * @param {ChannelName} channelName
+   * @private
+   */
   private _soundStarted(id: string, instance: IAudioInstance, channelName: ChannelName): void {
     // Logger.log(`${id} started in ${channelName} channel`);
     this.onSoundStarted.emit({ id, instance, channelName });
   }
 
+  /**
+   * Sound ended event handler. Emit the onSoundEnded signal.
+   * @param {string} id
+   * @param {IAudioInstance} instance
+   * @param {ChannelName} channelName
+   * @private
+   */
   private _soundEnded(id: string, instance: IAudioInstance, channelName: ChannelName): void {
     // Logger.log(`${id} ended in ${channelName} channel`);
     this.onSoundEnded.emit({ id, instance, channelName });
