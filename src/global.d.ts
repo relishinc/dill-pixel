ae9892027d25999fd83599cc0419caac5a8a4c10
anthonysapp <anthony@reli.sh>
2024-05-10 11:47:02 -0400

    fix: resizer tweak

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 export {};
-// export type RAPIER = import('@dimforge/rapier2d');
-// export type Matter = import('matter-js');
+export type RAPIER = import('@dimforge/rapier2d');
+export type Matter = import('matter-js');
 export type Spine = import('pixi-spine').Spine;
 export type SkeletonData = import('pixi-spine').SkeletonData;
 export type ISkeletonData = import('pixi-spine').ISkeletonData;
@@ -9,8 +9,8 @@ declare module 'react-fps-stats';
 declare module 'fontfaceobserver';
 
 declare global {
-  // const RAPIER: RAPIER;
-  // const Matter: Matter;
+  const RAPIER: RAPIER;
+  const Matter: Matter;
   const Spine: Spine;
   const SkeletonData: SkeletonData;
   const ISkeletonData: ISkeletonData;
