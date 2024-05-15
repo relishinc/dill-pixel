3c7c8c11cf54f57e87effc43f2d6f92af519bc10
anthonysapp <anthony@reli.sh>
2024-05-15 16:34:19 -0400

    docs: hello-world application config

--- docs/examples/hello-world/src/index.ts
+++ docs/examples/hello-world/src/index.ts
@@ -2,18 +2,11 @@ import { create } from 'dill-pixel';
 import { Application } from './Application';
 import './index.css';
 
-create(
-  Application,
-  {
-    antialias: false,
-    useNewResizeManager: true,
-    autoDensity: false,
-    resizeOptions: {
-      minSize: { width: 375, height: 700 },
-    },
-    resolution: Math.max(window.devicePixelRatio, 2),
-    backgroundColor: 0x0,
+create(Application, {
+  antialias: false,
+  resizeOptions: {
+    minSize: { width: 375, height: 700 },
   },
-  Application.containerID,
-  false,
-);
+  resolution: Math.max(window.devicePixelRatio, 2),
+  backgroundColor: 0x0,
+});
