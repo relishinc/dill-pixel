18dfca106ff0181248f2e77e2c5c52cc8b776e99
anthonysapp <anthony@reli.sh>
2024-06-03 12:19:40 -0400

    fix: formatting in hello line

--- src/hello.ts
+++ src/hello.ts
@@ -2,6 +2,6 @@ import { LogUtils } from './utils';
 import { pixiVersion, version } from './version';
 
 export function sayHello() {
-  const hello: string = `%cDill Pixel Game Framework v${version} - %chttps://dillpixel.io\nPixi.js v${pixiVersion}`;
-  console.log(hello, LogUtils.STYLE_RELISH, LogUtils.STYLE_RELISH_BOLD);
+  const hello: string = `%cDill Pixel Game Framework v${version} | %chttps://dillpixel.io | %cPixi.js v${pixiVersion}`;
+  console.log(hello, LogUtils.STYLE_RELISH, LogUtils.STYLE_RELISH_BOLD, LogUtils.STYLE_PINK_DARK);
 }
