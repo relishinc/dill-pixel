c3593cdb8f47cc433fe1c9c9219e2269b446e77b
anthonysapp <anthony@reli.sh>
2024-06-04 14:36:01 -0400

    fix: firebase adapter deaults hack

--- packages/storage-adapters/firebase/src/FirebaseAdapter.ts
+++ packages/storage-adapters/firebase/src/FirebaseAdapter.ts
@@ -22,12 +22,12 @@ interface DocumentResult extends DocumentData {
 }
 
 const defaultConfig: FirebaseOptions = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 };
 
 export interface IFirebaseAdapter extends IStorageAdapter {
