2e0918b4386f047ec8bf6c5fe51dd01e80724089
anthonysapp <anthony@reli.sh>
2024-06-05 17:18:35 -0400

    wip: ui scene, ui input

--- examples/src/scenes/UIScene.ts
+++ examples/src/scenes/UIScene.ts
@@ -1,12 +1,10 @@
 import { Input } from 'dill-pixel/ui';
 import { BaseScene } from '@/scenes/BaseScene';
-import { Texture } from 'pixi.js';
 import { FlexContainer } from 'dill-pixel';
 
 export class UIScene extends BaseScene {
   public title = 'UI';
   public subtitle = 'UI components';
-
   container: FlexContainer;
 
   public constructor() {
@@ -25,23 +23,53 @@ export class UIScene extends BaseScene {
     });
     const input = this.container.add.existing(
       new Input({
-        placeholder: { text: 'Enter first name', style: { fill: 0x666666 } },
-        bg: this.make.sprite({ asset: Texture.WHITE, width: 100, height: 50 }),
-        padding: [10, 20],
-        maxLength: 10,
+        input: { style: { align: 'left', fontSize: 30 } },
+        placeholder: { text: 'Align left', alpha: 0.5, style: { fill: 0x666666 } },
+        minWidth: 300,
+        padding: [12, 15],
       }),
       { label: 'input' },
     );
+
     const input2 = this.container.add.existing(
       new Input({
-        placeholder: { text: 'Enter last name', style: { fill: 0x666666 } },
-        bg: this.make.sprite({ asset: Texture.WHITE, width: 100, height: 50 }),
-        padding: [10, 20],
+        input: { style: { align: 'center' } },
+        placeholder: { text: 'Align center', style: { fill: 0x666666 } },
+
+        padding: [12, 15],
       }),
       { label: 'input2' },
     );
+    const input3 = this.container.add.existing(
+      new Input({
+        input: { style: { align: 'right' } },
+        placeholder: { text: 'Align right', style: { fill: 0x666666 } },
+        padding: [12, 15],
+      }),
+      { label: 'input3' },
+    );
+
+    const input4 = this.container.add.existing(
+      new Input({
+        input: { style: { align: 'left' } },
+        placeholder: { text: 'Disallow numbers', style: { fill: 0x666666 } },
+        padding: [12, 15],
+        pattern: '[^a-z]*',
+      }),
+      { label: 'input4' },
+    );
+    const input5 = this.container.add.existing(
+      new Input({
+        input: { style: { align: 'left' } },
+        placeholder: { text: 'Password', style: { fill: 0x666666 } },
+        padding: [12, 15],
+        type: 'password',
+        debug: true,
+      }),
+      { label: 'input5' },
+    );
 
     this.app.focus.add(input, this.id, true);
-    this.app.focus.add(input2, this.id);
+    this.app.focus.add([input2, input3, input4, input5], this.id);
   }
 }
