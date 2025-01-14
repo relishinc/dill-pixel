48ebae92809b91787f1205ce30050329c1fbb785
anthonysapp <anthonysapp@gmail.com>
2025-01-14 16:57:29 -0500

    chore: version bump on packages

--- packages/plugins/rive/src/version.ts
+++ packages/plugins/rive/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '4.1.1';
+export const version = '4.1.2';
 export const riveVersion = '2.18.0';
--- packages/storage-adapters/firebase/src/version.ts
+++ packages/storage-adapters/firebase/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '4.1.1';
+export const version = '4.1.2';
 export const firebaseVersion = '10.12.2';
