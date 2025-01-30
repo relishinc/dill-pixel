6800bbd99df7a4e4cf50f5077dbe60dd4350662b
anthonysapp <anthonysapp@gmail.com>
2025-01-30 13:47:14 -0500

    feat: new name

---
+++ packages/plugins/physics-crunch/src/types.ts
@@ -0,0 +1,65 @@
+import { PointLike, SizeLike } from 'dill-pixel';
+import { Container } from 'pixi.js';
+import { Actor } from './Actor';
+import { Sensor } from './Sensor';
+import { Solid } from './Solid';
+
+export interface Vector2 {
+  x: number;
+  y: number;
+}
+
+export interface Rectangle {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export type CollisionShape = 'rectangle';
+export type EntityData = {
+  [key: string]: any;
+};
+
+export interface PhysicsEntityConfig<D extends EntityData = EntityData> {
+  type?: PhysicsEntityType;
+  position?: PointLike;
+  size?: SizeLike;
+  x?: number;
+  y?: number;
+  width?: number;
+  height?: number;
+  restitution?: number;
+  view?: PhysicsEntityView;
+  data?: Partial<D>;
+}
+
+export interface CollisionResult {
+  collided: boolean;
+  normal?: Vector2;
+  penetration?: number;
+  solid: Solid;
+}
+
+export interface SensorOverlap {
+  type: `${PhysicsEntityType}|${PhysicsEntityType}`;
+  actor: Actor;
+  sensor: Sensor;
+}
+
+export interface CollisionResult {
+  collided: boolean;
+  normal?: Vector2;
+  penetration?: number;
+  solid: Solid;
+}
+
+export interface Collision {
+  type: `${PhysicsEntityType}|${PhysicsEntityType}`;
+  entity1: Actor | Sensor;
+  entity2: Actor | Solid;
+  result: CollisionResult;
+}
+
+export type PhysicsEntityView = Container;
+export type PhysicsEntityType = 'Actor' | 'Solid' | string;
