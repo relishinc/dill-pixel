7b4f541574303f84ced79b6ac10fe6e4db9f6e5c
anthonysapp <anthonysapp@gmail.com>
2025-02-18 15:09:49 -0500

    chore: fixed typo

--- packages/framework/templates/app/default/src/__APPLICATION_NAME__.ts
+++ packages/framework/templates/app/default/src/__APPLICATION_NAME__.ts
@@ -2,5 +2,5 @@ import { Application } from 'dill-pixel';
 
 import { type ActionTypes, type Contexts, type Data } from './dill-pixel.config';
 
-// TODO: Add custom applicartion logic here (optional)
+// TODO: Add custom application logic here (optional)
 export class __APPLICATION_NAME__ extends Application<Data, Contexts, ActionTypes> {}
