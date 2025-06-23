8b2baf2d7e0aa86f90c90a9e94905872e917a612
anthonysapp <anthonysapp@gmail.com>
2025-06-23 12:00:37 -0400

    refactor: moved packages/examples to apps/kitchen-sink

--- apps/examples/hello-world/README.md
+++
@@ -1,8 +0,0 @@
-# My Dill Pixel Application
-
-## Development
-
-- run `pnpm install` to install dependencies
-- run `pnpm dev` to start the vite development server
-- make changes to the code
-- see them reflected at http://localhost:3000
---
+++ apps/kitchen-sink/vite.config.mjs
@@ -0,0 +1,24 @@
+// import { withPWA } from 'dill-pixel/config/vite';
+import { extendConfig } from 'dill-pixel/config/vite';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default extendConfig({
+  resolve: {
+    alias: {
+      'dill-pixel': path.resolve(__dirname, '../../packages/framework/src'),
+      '@dill-pixel/plugin-snap-physics': path.resolve(__dirname, '../../packages/plugins/physics-snap/src'),
+      '@dill-pixel/plugin-matter-physics': path.resolve(__dirname, '../../packages/plugins/physics-matter/src'),
+      '@dill-pixel/plugin-crunch-physics': path.resolve(__dirname, '../../packages/plugins/physics-crunch/src'),
+      '@dill-pixel/plugin-springroll': path.resolve(__dirname, '../../packages/plugins/springroll/src'),
+      '@dill-pixel/plugin-google-analytics': path.resolve(__dirname, '../../packages/plugins/google-analytics/src'),
+      '@dill-pixel/plugin-rive': path.resolve(__dirname, '../../packages/plugins/rive/src'),
+      '@dill-pixel/plugin-rollbar': path.resolve(__dirname, '../../packages/plugins/rollbar/src'),
+      '@dill-pixel/storage-adapter-firebase': path.resolve(__dirname, '../../packages/storage-adapters/firebase/src'),
+      '@dill-pixel/storage-adapter-supabase': path.resolve(__dirname, '../../packages/storage-adapters/supabase/src'),
+    },
+  },
+});
